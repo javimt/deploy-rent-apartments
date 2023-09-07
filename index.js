545f03d9704fc93de8ed750b2fe7065b871bd1d0
javimt <javiergp_19@outlook.com>
2023-09-07 13:22:12 -0500

    4

--- index.js
+++ index.js
@@ -8,39 +8,41 @@ const { checkExpiredRents } = require("./src/controllers/rentExpiration");
 
 const port = process.env.PORT || 3001;
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
-cron.schedule("0 12 * * *", () => {
-  console.log("Verificando alquileres vencidos...");
-  checkExpiredRents();
-});
+const startServer = async () => {
+  const app = express();
+  app.use(morgan("dev"));
+  app.use(express.json());
+  app.use(cors());
 
-const app = express();
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cors());
+  app.use("/", router);
 
-app.use("/", router);
-
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to Furnished Apartments Medellin")
-})
-
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  res.status(status).json({ status: "error", message });
-  next();
-});
-
-connection.sync({ force: false })
-  .then(() => {
-    app.listen(port, () => {
-      console.info(`Server is running on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error syncing database:', error);
+  app.get("/", (req, res) => {
+    res.status(200).send("Welcome to Furnished Apartments Medellin");
   });
+
+  app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    res.status(status).json({ status: "error", message });
+    next();
+  });
+
+  try {
+    await connection.sync({ force: false });
+    console.info(`Server is listening on port ${port}`);
+    app.listen(port);
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+
+  cron.schedule("0 12 * * *", () => {
+    console.log("Verifying expired rentals...");
+    checkExpiredRents();
+  });
+};
+
+startServer();
