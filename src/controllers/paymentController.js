3cf5472fd861f5a052165ed78b66fd778e520fe6
javimt <javiergp_19@outlook.com>
2024-01-30 11:55:02 -0500

    payment controller

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,27 +1,30 @@
-//const { PayPal } = require("paypal-js");
 const axios = require("axios");
 
 module.exports = {
   createOrder: async (req, res) => {
-    const order = {
+    const {price, userId, apartmentId} = req.body;
+    try {
+      const order = {
       intent: "CAPTURE",
       purchase_units: [
         {
           amount: {
             currency_code: "USD",
-            value: "950.00"
+            value: price
           },
         },
       ],
       application_context: {
         brand_name: "Furnished Apartments Medellin",
-        landing_page: "NO_PREFERENCE",
+        landing_page: "LOGIN",
         user_action: "PAY_NOW",
-        return_url: `${process.env.BACK_URL}/capture-order`,
+        return_url: `${process.env.BACK_URL}/checkout/capture-order`,
         cancel_url: `${process.env.BACK_URL}/cancel-order`
       }
     }
-
+    } catch (error) {
+      
+    }
     //const api = 'https://api-m.paypal.com/v2/checkout/orders'
     const params = new URLSearchParams();
     params.append('grant_type', 'client_credentials');
@@ -33,12 +36,12 @@ module.exports = {
       }
     });
  //console.log(process.env.PAYPAL_API)
-    axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders`, order, {
+    const response = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders`, order, {
       headers: {
         Authorization: `Bearer ${access_token}`
       }
     })
-    return res.json('create order')
+    res.json('create order')
   },
 
   rejectOrder: async (req, res) => {
